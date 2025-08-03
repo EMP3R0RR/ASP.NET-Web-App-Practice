96387ad1358dd87966a29030c1dcc4716ef17da1
Syed Musaeed Hossain <syedpunno@gmail.com>
2025-08-03 13:37:55 +0600

    Refactor ProjectDetailsDTO properties and constructor
    Changed Supervisor and Status properties to single objects
    instead of lists. Updated constructor to reflect these changes
    by removing list initializations while keeping Members initialization.

--- Test Application/DTO/ProjectDetailsDTO.cs
+++ Test Application/DTO/ProjectDetailsDTO.cs
@@ -7,15 +7,15 @@ namespace Test_Application.DTO
 {
     public class ProjectDetailsDTO : ProjectDTO
     {
-        public List<SupervisorDTO> Supervisor { get; set; }
+        public SupervisorDTO Supervisor { get; set; }
         public List<MemberDTO> Members { get; set; }
 
-        public List<ProjectStatusDTO> Status { get; set; }
+        public ProjectStatusDTO Status { get; set; }
         public ProjectDetailsDTO()
         {
-            Supervisor = new List<SupervisorDTO>();
+            
             Members = new List<MemberDTO>();
-            Status = new List<ProjectStatusDTO>();
+            
         }
 
         
